0e669239f90a71c3273be5261c1a2b813507c8f5
Lee-GiHeung <dlrlgmd25@gmail.com>
2024-10-20 17:21:36 +0900

    first commit

---
+++ underscore.js
@@ -0,0 +1,6 @@
+var _ = require('underscore');
+var arr = [3, 6, 9, 1, 12];
+console.log(arr[0]);            // 3
+console.log(_.first(arr))       // 3
+console.log(arr[arr.length-1])  // 12
+console.log(_.last(arr));       // 12
