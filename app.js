51b6a4cd2c56c47e0ce258148eccb5534b4d0f3a
Lee-GiHeung <dlrlgmd25@gmail.com>
2024-10-28 15:40:32 +0900

    정적 파일 서비스 하는 법

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var app = express();
+app.use(express.static('public'));
 
 app.get('/', function(req, res){
     res.send('<h2>Hello home page</h2>');
+});
+app.get('/route', function(req, res) {
+    res.send('Hello Router, <img src="/ex.png">')
 });
 app.get('/login', function(req, res){
     res.send('<h1>Login please</h1>');
