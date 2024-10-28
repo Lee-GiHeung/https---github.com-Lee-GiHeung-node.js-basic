816bcab1a586e1c0cf6402b32e6c3f9551062df9
Lee-GiHeung <dlrlgmd25@gmail.com>
2024-10-28 16:59:24 +0900

    웹페이지 표현하는 방법

--- app.js
+++ app.js
@@ -5,8 +5,31 @@ app.use(express.static('public'));
 app.get('/', function(req, res){
     res.send('<h2>Hello home page</h2>');
 });
+app.get('/dynamic', function(req, res) {
+    var lis = '';
+    for(var i=0; i<5; i++) {
+        lis = lis + '<li>coding</li>';
+    }
+    var time = Date();
+    var output = `
+    <!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <title></title>
+        </head>
+        <body>
+            Hello, Dynamic!
+            <ul>
+                ${lis}
+            </ul> 
+            ${time}
+        </body>
+    </html>`;
+    res.send(output);
+});
 app.get('/route', function(req, res) {
-    res.send('Hello Router, <img src="/ex.png">')
+    res.send('Hello Router, <img src="/route/ex.png">')
 });
 app.get('/login', function(req, res){
     res.send('<h1>Login please</h1>');
