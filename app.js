53a80a4eab97af80e226fd8c2c030284300a7536
Lee-GiHeung <dlrlgmd25@gmail.com>
2024-10-29 17:18:55 +0900

    시멘틱 URL

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ app.locals.pretty = true;
 app.set('view engine', 'jade');
 app.set('views', './views');
 app.use(express.static('public'));
-app.get('/topic', function(req, res){
+app.get('/topic/:id', function(req, res){
     var topics = [
         'JavaScript is...',
         'NodeJs is...',
@@ -14,9 +14,12 @@ app.get('/topic', function(req, res){
     <a href="/topic?id=0">JavaScript</a><br>
     <a href="/topic?id=1">NodeJs</a><br>
     <a href="/topic?id=2">Express</a><br><br>
-    ${topics[req.query.id]}
+    ${topics[req.params.id]}
     `
     res.send(output);
+});
+app.get('/topic/:id/:mode', function(req, res){
+    res.send(req.params.id+','+req.params.mode)
 });
 
 app.get('/template', function(req, res) {
