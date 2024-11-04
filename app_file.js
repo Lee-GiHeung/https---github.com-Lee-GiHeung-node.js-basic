7285497eb76df4f44049f376bec694a70e4dfb68
Lee-GiHeung <dlrlgmd25@gmail.com>
2024-11-04 23:11:17 +0900

    본문 저장

--- app_file.js
+++ app_file.js
@@ -1,5 +1,8 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var fs = require('fs');
 var app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
 app.locals.pretty = true;
 app.set('views', './views_file');
 app.set('view engine', 'jade');
@@ -7,7 +10,15 @@ app.get('/topic/new', function(req, res) {
     res.render('new');
 });
 app.post('/topic', function(req, res) {
-    res.send('hi topic post');
+    var title = req.body.title;
+    var description = req.body.description;
+    fs.writeFile('data/'+title,description, function(err) {
+        if(err){
+            console.log(err);
+            res.status(500).send('Internal Server Error');
+        }
+        res.send('Success!');
+    });
 });
 app.listen(3000, function(req, res) {
     console.log('Connected, 3000 port');
